198e318c4cce749b79bd6c4dcd17d377014d1bc2
adam.hutchison <adam.hutchison@netsells.co.uk>
2018-05-31 22:19:10 +0100

    Debug images not showing

--- resources/views/partials/app.blade.php
+++ resources/views/partials/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Andrew Hamilton</title>
+    <title>Andrew Hamilton Design</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300" rel="stylesheet">
