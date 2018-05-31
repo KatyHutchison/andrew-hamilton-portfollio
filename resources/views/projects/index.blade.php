b4fd05b359e6a33271e632d9df294f861b5dc96b
adam.hutchison <adam.hutchison@netsells.co.uk>
2018-05-31 22:31:40 +0100

    Debug images not showing

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -7,7 +7,7 @@
                 @if ($project->slice(0,1)->first())
                     <div class="col-md-4 thumbnail-height">
                         <a href="/project/{{$project->slice(0,1)->first()->id}}">
-                            <img class="img-fluid" src={{ asset($project->slice(0,1)->first()->thumbnailImage->path) }} alt="">
+                            <img class="img-fluid" src={{ storage_path($project->slice(0,1)->first()->thumbnailImage->path) }} alt="">
                         </a>
                         <div class="text-center pt-2">
                             {{ $project->slice(0,1)->first()->thumbnailImage->label }}
@@ -17,7 +17,7 @@
                 @if ($project->slice(1,1)->first())
                     <div class="col-md-4 thumbnail-height">
                         <a href="/project/{{$project->slice(1,1)->first()->id}}">
-                            <img class="img-fluid" src={{ asset($project->slice(1,1)->first()->thumbnailImage->path) }} alt="">
+                            <img class="img-fluid" src={{ storage_path($project->slice(1,1)->first()->thumbnailImage->path) }} alt="">
                         </a>
                         <div class="text-center pt-2">
                             {{ $project->slice(0,1)->first()->thumbnailImage->label }}
@@ -27,7 +27,7 @@
                 @if ($project->slice(2,1)->first())
                     <div class="col-md-4 thumbnail-height">
                         <a href="/project/{{$project->slice(2,1)->first()->id}}">
-                            <img class="img-fluid" src={{ asset($project->slice(2,1)->first()->thumbnailImage->path) }} alt="">
+                            <img class="img-fluid" src={{ storage_path($project->slice(2,1)->first()->thumbnailImage->path) }} alt="">
                         </a>
                         <div class="text-center pt-2">
                             {{ $project->slice(0,1)->first()->thumbnailImage->label }}
