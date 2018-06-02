afd4af4f5105f983d192a1e8ee5afbf9861ec01d
adam.hutchison <adam.hutchison@netsells.co.uk>
2018-06-03 00:40:28 +0100

    Adds hamburger menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,22 @@
 <header>
-    <div class="row nav-top">   
+    <div class="row nav-top">
         <div class="col-md-5">
             <a href="/">
                 <h1>ANDREW HAMILTON</h1>
                 <h2>FURNITURE AND PRODUCT DESIGN</h2>
             </a>
         </div>
-    
-        <nav>
-            <a href="/">WORK</a>
-            <a href="/about">ABOUT</a>
-            <a href="/">CONTACT</a>
+
+        <nav class="navbar-expand-lg navbar navbar-light navbar-style">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+                    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a href="/">WORK</a>
+                <a href="/about">ABOUT</a>
+                <a href="/">CONTACT</a>
+            </div>
         </nav>
     </div>
 </header>
