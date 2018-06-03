271ed01d282d2c52d2cd77f305f67e2fa5f3f51e
adam.hutchison <adam.hutchison@netsells.co.uk>
2018-06-03 09:37:44 +0100

    Adds css for tablet mode

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar-expand-lg navbar navbar-light navbar-style">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
-            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarToggler">
